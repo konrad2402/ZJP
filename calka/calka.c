582562cdc1ac15a9cc3b2510b0b3b65bb081945c
kmieszala <mkonrad14@gmail.com>
2013-10-23 11:06:36 +0200

    calka na tip top :)

--- calka/calka.c
+++ calka/calka.c
@@ -1,12 +1,8 @@
 /* calka dla x=(-2;2) dla n=512 */
-// dla n=512 program działa bardzo szybko, dlatego zmieniam 512 na 1 000 000, żeby lepiej zaobserwowac zmiany czasowe
+// dla n=512 program działa bardzo szybko, dlatego zmieniam 512 na 1 000 000, żeby lepiej zaobserwowac czas działania
 #include<stdio.h>
 #include<math.h>
 #include<time.h>
-
-#define N 1000000
-#define X -2
-#define Y 2
 
 /* e^-x^2 */
 double f (double x){
@@ -14,19 +10,19 @@ double f (double x){
 }
 
 /* f(x) = ([f(x_0*h) + f(x_1*h) + ... + f(x_n-1*h)]/2) *h */
-int main(){
+int main(int argc, char **argv){
     clock_t start, stop;
 	start=clock();
 	double i, time;
 	double calka = 0 ;
-	double x=X, y=Y, n =N;
+	double x=atoi(argv[2]), y=atoi(argv[3]), n = atoi(argv[1]);
 	double h = (y-x)/n;
 
 	for(i=1;i<(n-1);i++){
         calka += (f(x+i*h) + f(x+(i+1.0)*h))/2.0;
 	}
     calka = calka * h;
-    printf("\nCalka dla x=(%d;%d) i n=%d wynosi: %f \n", X, Y, N, calka);
+    printf("\nCalka dla x=(%.0f;%.0f) i n=%d wynosi: %f \n", x, y, atoi(argv[1]), calka);
 	stop=clock();
     time=(stop - start);
 	printf("Czas wykonania algorytmu to: %ld\n", stop-start);
