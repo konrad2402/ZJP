d5846483c4b21e99fae32966e34c65c968d499d8
kmieszala <mkonrad14@gmail.com>
2013-10-23 14:46:33 +0200

    mpimacierz1

--- macierz/rozwiazanie.c
+++ macierz/rozwiazanie.c
@@ -10,15 +10,16 @@ int randome(){
 }
 void wypisz(long int x){
         if (x >= 1000) printf("%ld ", x);
-        else if (x >= 100) p
-		printf("%ld  ", x);
+        else if (x >= 100) printf("%ld  ", x);
         else if (x >= 10) printf("%ld   ", x);
         else printf("%ld    ", x);
 }
 
 int main(int argc, char **argv){
 		int rank; // dany proces
+		int tmp;// do sprawdzenia czy ilosc procesow jest rowna wielkosci macierzy
 		int namelen;
+		long int x;
 		int numprocs; // ilosc procesow
 		long int i = 0, l=x+2;
 		int k = 0, xx = 0;
@@ -28,45 +29,53 @@ int main(int argc, char **argv){
 		/* Message tag */
 		int tag = 1;
 		//MPI_Status status;
-		long int x, tmp;
         x = atoi(argv[1]);
-		i = x;
-		/* LOSOWANIE LICZB I WYPELNIENIE MACIERZY I WEKTORA */
-		for(i;i<x;i++){
-			int j = 0;
-			wektor[i] = randome();
-			for(j;j<x;j++){
-				macierz1[i][j] = randome();
-			}
-		}
 		MPI_Init(&argc, &argv);
 		MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 		MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
-	
-		if(numprocs==x) {
-			int l;
-			l = x+1;
+		tmp = x%numprocs; // wyliczenie różnicy między ilosica procesow i wielkosica macierzy
+		if(tmp != 0) {
+		printf("%d   tmp\n", tmp);
+			tmp = numprocs-tmp;		
+		printf("%d   tmp\n", tmp);	
 		}
-		else {
-			int l = x/numprocs;
-			l = l*numprocs;
-			l = x-l;
-			printf("%d LOLOL \n\n", l);
+		i = 0;
+		if(rank == 0){
+			/* LOSOWANIE LICZB I WYPELNIENIE MACIERZY I WEKTORA */
+			for(i;i<x;i++){
+				int j = 0;
+				wektor[i] = randome();
+				for(j;j<x;j++){
+					macierz1[i][j] = randome();
+				}
+			}	
+			// dopisanie zer
+			if(tmp != 0) {
+				for(tmp; tmp>=0; tmp--){
+					int j = 0;
+					wektor[i] = 0;
+					for(j;j<x;j++){
+						macierz1[i][j] = 0;
+					}
+				}
+			}
 		}
-			
+		
 	/* LICZENIE MACIERZY */
 		for(k = 0; k<x; k++){
-				for(i = 0;i<x;i++){
-						int j = 0;
-						wynik[k][i] = 0;
-						for(j;j<x;j++){
-								xx = macierz1[k][j] * wektor[j];
-								wynik[k][i] = wynik[k][i] + xx;
-						}
+			int begin = (rank-1)*n/size;
+			int end = (rank)*n/size;  
+			for(i = begin; i < end; i++){
+				int j = 0;
+				wynik[k][i] = 0;
+				for(j;j<x;j++){
+					xx = macierz1[k][j] * wektor[j];
+					wynik[k][i] = wynik[k][i] + xx;
 				}
+			}
 		}
 				printf("\n");
-		/* WYPISANIE MACIERZY */
+		/* WYPISANIE MACIERZY 
 		i = 0;
 		for(i;i<x;i++){
 				int j = 0;
@@ -84,7 +93,7 @@ int main(int argc, char **argv){
 						wypisz(wynik[i][j]);
 				}
 				printf("\n");
-		}
+		}*/
 		MPI_Finalize();
 		return 0;
 }
